7513b247de30e0dfeb80e4bf473fc2b408751d52
Muhammad Khalil <mk0180770@gmail.com>
2025-09-15 13:19:51 +0500

    Update route.ts - switch ip to actual link

--- app/api/proxy/[...path]/route.ts
+++ app/api/proxy/[...path]/route.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server"
 
-const API_BASE_URL = "http://192.168.0.103:8084/api"
+const API_BASE_URL = "http://api.jariya.net/api"
 
 export async function GET(request: NextRequest, { params }: { params: { path: string[] } }) {
   try {
